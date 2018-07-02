c47f76e9a4fb5e022b61e09342f36960b9759f61
Raúl Mosquera <raulv.mosquera@gmail.com>
2018-07-02 15:58:47 -0500

    Initial commit

---
+++ test.py
@@ -0,0 +1,14 @@
+n = int(input())
+student_marks = {}
+
+#for _ in range(n):
+#    name, *line = input().split()
+#    scores = list(map(float, line))
+#    student_marks[name] = scores
+#query_name = input()
+
+sum = 5
+#for elemen in student_marks[query_name]:
+#    sum = sum + elemen
+
+print("%.2f" % sum)
